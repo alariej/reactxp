dce678fdb3a2b8c03a15ff48d59cc62264169beb
alariej <alaj@gmx.net>
2021-11-26 14:49:52 +0100

    removed windows and macos

--- dist/macos/View.d.ts
+++
@@ -1,6 +0,0 @@
-import { Types } from '../common/Interfaces';
-import { View as ViewCommon } from '../native-common/View';
-export declare class View extends ViewCommon {
-    protected _buildInternalProps(props: Types.ViewProps): void;
-}
-export default View;
